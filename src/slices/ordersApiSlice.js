25e745195aeada90b7ef78ce838ff613c72af748
Houssem Eddine ELGAIED <houssemelgaied@gmail.com>
2024-12-19 22:31:03 +0100

    Update ordersApiSlice.js

--- src/slices/ordersApiSlice.js
+++ src/slices/ordersApiSlice.js
@@ -7,7 +7,7 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
       query: (order) => ({
         url: ORDERS_URL,
         method: 'POST',
-        credentials: 'include'
+        credentials: 'include',
         body: { ...order }
       }),
       invalidatesTags: ['Order']
