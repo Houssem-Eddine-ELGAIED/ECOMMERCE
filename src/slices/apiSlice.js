90ee812da81ec445ab00e138898f6f83e0f0e4bd
Houssem Eddine ELGAIED <houssemelgaied@gmail.com>
2024-12-19 22:41:20 +0100

    Update apiSlice.js

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -4,6 +4,7 @@ import { BASE_URL } from '../constants';
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   tagTypes: ['User', 'Product', 'Order'],
+ credentials: 'include',
   endpoints: builder => ({})
 });
                                                               
